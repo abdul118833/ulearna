944437a4fb62681a4bfc0b91ef1200288871e2f2
Abdul Majid <abdul@infosun.co.uk>
2024-08-16 01:44:24 +0500

    UI Scrollbar Pushed to main

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import WeatherCard from '@/components/WhetherCard';
-import useWeather from '@/hooks/useWhether';
-import { initialCities } from '@/constants';
-import Searchbar from '@/components/Searchbar';
-import Loader from '@/components/Loader';
-
+import React, { useEffect, useState } from "react";
+import WeatherCard from "@/components/WhetherCard";
+import useWeather from "@/hooks/useWhether";
+import { initialCities } from "@/constants";
+import Searchbar from "@/components/Searchbar";
+import Loader from "@/components/Loader";
 
 const WeatherDashboard = () => {
-  const {
-    weatherData,
-    cities,
-    loading,
-    fetchWeatherForCities,
-    setCities,
-  } = useWeather(initialCities); // Use the custom hook
+  const { weatherData, cities, loading, fetchWeatherForCities, setCities } =
+    useWeather(initialCities); // Use the custom hook
 
   const [searchQuery, setSearchQuery] = useState("");
 
@@ -52,26 +46,28 @@ const WeatherDashboard = () => {
   }, []);
 
   return (
-    <div className="container mx-auto w-[100vw] h-[100vh] overflow-auto">
-      <h1 className="text-4xl font-bold text-center my-8 text-white">
-        Weather Dashboard
-      </h1>
-      <Searchbar {...searchBarProps} />
-      {loading ? (
-        <Loader />
-      ) : cities.length > 0 ? (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-4">
-          {cities.map((city) => (
-            <WeatherCard
-              key={city}
-              city={city}
-              weatherData={weatherData[city] || {}}
-            />
-          ))}
-        </div>
-      ) : (
-        <p className="text-center font-bold text-white">No Data Found</p>
-      )}
+    <div className="w-[100vw] h-[100vh] overflow-auto">
+      <div className="container mx-auto">
+        <h1 className="text-4xl font-bold text-center my-8 text-white">
+          Weather Dashboard
+        </h1>
+        <Searchbar {...searchBarProps} />
+        {loading ? (
+          <Loader />
+        ) : cities.length > 0 ? (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-4">
+            {cities.map((city) => (
+              <WeatherCard
+                key={city}
+                city={city}
+                weatherData={weatherData[city] || {}}
+              />
+            ))}
+          </div>
+        ) : (
+          <p className="text-center font-bold text-white">No Data Found</p>
+        )}
+      </div>
     </div>
   );
 };
